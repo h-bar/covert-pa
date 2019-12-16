603abd91be023c7172108083456dbf20d098e524
Yan Zhang <663yan@gmail.com>
2019-12-16 03:29:56 -0500

    transmit random string

--- spi.cpp
+++ spi.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <time.h>
 
 #include <set>
 #include <vector>
@@ -10,19 +10,21 @@
 #include <thread>
 #include <random>
 
+
 #include "prime.cpp"
 #include "eset.cpp"
 
 // #define clock_cycle 150	
 #define clock_cycle 50000
+#define settle_time 2000000
 
 #define buffer_size 100
-#define buffer_threshold 10
+#define buffer_threshold 30
 
 #define sample_size 100
-#define sample_threshold 10
+#define sample_threshold 20
 
-#define nway_in			8
+#define nway_in			12
 #define nway_out		12
 
 
@@ -82,7 +84,7 @@ int slave() {
 		perror("Error in opening file");
 		return(-1);
 	} 
-	char data = 0;
+
 	vector<set<char *>> e_sets = esets(0);
 	int slice = scan(e_sets, ss);
 
@@ -101,7 +103,8 @@ int slave() {
 	int counter = 0;
 
 	char **clk_addrs = construct_addrs(e_sets[slice], clk);
-
+	
+	bitset<8> data_buffer(0);
 	int clock_value = 0;
 	int value_in = 0;
 	while (1) {
@@ -112,7 +115,7 @@ int slave() {
 				clk_buffer[0] = sample(&clk_addrs, sample_size, sample_threshold);
 			} while (clk_buffer.count() > buffer_threshold && ss_buffer.count() > buffer_threshold);
 
-			data += (mosi_buffer.count() < buffer_size - buffer_threshold) << counter;
+			data_buffer[counter] = (mosi_buffer.count() < buffer_size - buffer_threshold);
 			counter ++;
 			do {
 				clk_buffer <<= 1;
@@ -121,21 +124,13 @@ int slave() {
 		}
 		// // fputc(data, fp);
 		// // fclose(fp);
-		printf("%c", data);
-		data = 0;
+		printf("%c", (char)data_buffer.to_ulong());
+		data_buffer.reset();
 		counter = 0;
 	}
 }
 
 int master() {
-	FILE *fp = fopen("../test/file.txt", "r");\
-	if(fp == NULL) {
-		perror("Error in opening file");
-		return(-1);
-	} 
-	char data;
-
-
   set<char *> e_set = esets(0)[0];
 
 	int mosi_value = 0;
@@ -146,37 +141,42 @@ int master() {
 	char **clk_addrs = construct_addrs(e_set, clk);
 	char **null_addrs = construct_addrs(e_set, clk + 2);
 	int count = 0;
-	char data_sent = 0;
+	int nbytes = 0;
+	char data = 0;
 	int half_clock_cycle = clock_cycle / 2;
+	int i, j;
+
+	printf("Press enter to start transmission");
+	getchar();
+	unsigned long start_t = (unsigned long)time(NULL);
 	while (1) {
-		// if (feof(fp)) {fseek( fp, 0, SEEK_SET );}
-		// data = fgetc(fp);
-		data = getchar();
 		ss_value = 0;
-		for (int i = 0; i < 8; i++) {
-			mosi_value = data & 1;
-			count = 0;
-			while (count ++ < half_clock_cycle) {
-				pa_prime(null_addrs, nway_out);
+		for (i = 0; i < 1; i++) {
+			data = ' ' + (rand() % 96);
+			for (j = 0; j < 8; j++) {
+				mosi_value = data & 1;
+				count = 0;
+				while (count ++ < half_clock_cycle) {
+					pa_prime(null_addrs, nway_out);
+				}
+				
+				count = 0;
+				while (count ++ < half_clock_cycle) {
+					pa_prime(clk_addrs, nway_out);
+				}
+				data >>= 1;
 			}
-			
-			count = 0;
-			while (count ++ < half_clock_cycle) {
-				pa_prime(clk_addrs, nway_out);
-			}
-			data >>= 1;
+			nbytes++;
 		}
 		ss_value = 1;
+
 		count = 0;
-		while (count ++ < half_clock_cycle) {
-			pa_prime(null_addrs, nway_out);
-		}
-		
-		count = 0;
-		while (count ++ < half_clock_cycle) {
-			pa_prime(clk_addrs, nway_out);
-		}
+		while (count ++ < settle_time) {}
 	}
+	unsigned long end_t = (unsigned long)time(NULL);
+	printf("%d bytes sent in %lu seconds -> %f bytes/sec\n", nbytes, (end_t - start_t), (float)nbytes / (end_t - start_t));
+	while(1);
+
 }
 
 int main(int argc, char *argv[]) {
